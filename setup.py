3089d5e607d503749cca5254ea1f4a6314466b67
Matthew Hooker <mwhooker@gmail.com>
2012-01-13 20:14:23 -0500

    setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name='scribe_logger',
+    version='1.0',
+    description='Scribe log writer and logging handler.',
+    author='Chris Goffinet',
+    author_email='cg@chrisgoffinet.com',
+    maintainer='Matthew Hooker',
+    maintainer_email='mwhooker@gmail.com',
+    url='https://github.com/mwhooker/python-scribe-logger',
+    packages=['scribe_logger'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    keywords=[
+        'scribe',
+        'logging'
+    ]
+)

0269cfb1d1207758e285fb3140f4335b1ec6073b
shishusaiwai <564630776@qq.com>
2012-12-20 11:32:43 +0800

    add handle error

--- scribe_logger/logger.py
+++ scribe_logger/logger.py
@@ -6,12 +6,12 @@ This class handles overwriting logging to send to a scribe instance.
 from scribe_logger.writer import ScribeWriter
 import logging
 import logging.handlers
-import sys
 
 
 class ScribeLogHandler(logging.Handler, ScribeWriter):
     """"""
-    def __init__(self, category=None, extra=None, host='127.0.0.1', port=1463):
+    def __init__(self, category=None, extra=None,
+            backup_file="", host='127.0.0.1', port=1463):
         logging.Handler.__init__(self)
 
         if category:
@@ -21,6 +21,10 @@ class ScribeLogHandler(logging.Handler, ScribeWriter):
             self.extra = ' '.join(extra)
         else:
             self.extra = ''
+
+        self.backup_file = backup_file
+        if not self.backup_file:
+            raise AttributeError("No backup file supplied.")
 
         ScribeWriter.__init__(self, host, port, self.category)
 
@@ -44,3 +48,12 @@ class ScribeLogHandler(logging.Handler, ScribeWriter):
 
     def flush(self):
         pass
+
+    def handleError(self, record):
+        with open(self.backup_file, 'w') as f:
+            msg = record.getMessage()
+            if len(msg) >= 1:
+                tail = msg[-1]
+                if tail != "\n":
+                    msg += "\n"
+            f.write(msg)
